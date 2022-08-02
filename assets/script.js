db850a3e80b34c711e055ca2eb5d012d25f93220
gregali9311 <gjali@asu.edu>
2022-08-01 20:14:29 -0700

    updates to javascript- api calls

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,32 @@
+var starbtn = document.getElementById("startButton");
 
 
-var requestUrl = "https://opentdb.com/api.php?amount=25&type=multiple";
+// var questcat ="11";
+var difcat = "Any Category";
+
+var questlength = "";
+
+var requestUrl = "";
+
+// if(questcat == "Any Category" && difcat =="Any Type"){
+//     requesturl = "https://opentdb.com/api.php?amount=25&type=multiple";
+// } else if (questcat == "Any Category" && difcat !=="Any Type"){
+//     requestUrl= "https://opentdb.com/api.php?amount=25&difficulty=" + difcat +"&type=multiple";
+// } else if (questcat !== "Any Category" && difcat !== "Any Type"){
+//     requestUrl = "https://opentdb.com/api.php?amount=25&category=" + questcat + "&difficulty=" + difcat+ "&type=multiple";
+// } else if (questcat !== "Any Category" && difcat =="Any Type"){
+//     requestUrl= "https://opentdb.com/api.php?amount=25&category=" + questcat + "&type=multiple";
+// }
+
+if(difcat == "Any Type"){
+    requesturl = "https://opentdb.com/api.php?amount=" + questlength + "&type=multiple";
+} else if (difcat !=="Any Type"){
+    requestUrl= "https://opentdb.com/api.php?amount=" + questlength +"&difficulty=" + difcat +"&type=multiple";
+};
+
+
+
+console.log(requestUrl)
 
 fetch(requestUrl)
 .then(function (response){
@@ -9,6 +35,26 @@ fetch(requestUrl)
 })
 .then(function (data) {
     console.log(data);
-
-    
+    randomquestions(data);
 });
+
+var randomquestion = Math.floor(Math.random() * 25);
+
+var randomquestions = function(data){
+    console.log(data.results[randomquestion].question);
+    console.log(randomquestion)
+}
+
+
+starbtn.addEventListener ("click", function(){
+    starbtn.style.visibility="hidden";
+    
+})
+
+
+// Category: 
+// Any Category-- Removed
+// General Knowledge--9
+// Entertainment: Books--10 
+// Entertainment: Music--11
+// Difficulty: easy=easy, medium=medium, hard= hard
