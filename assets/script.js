a8efef1f3572dc12aad56039d7cde6531a9b6c9c
James Zagorski <james_zagorski@yahoo.com>
2022-08-09 01:59:27 -0700

    CSS Fixes2 8-2

--- assets/script.js
+++ assets/script.js
@@ -118,7 +118,7 @@ starbtn.addEventListener ("click", function(){
 
         
         function hidquestion(){
-            starbtn.style.display ="inline-block";
+            starbtn.style.display ="inline-flex";
             list.remove();
             newh1.remove();
 
@@ -127,7 +127,7 @@ starbtn.addEventListener ("click", function(){
         answ1.addEventListener("click", function(){
             if(actualresponse1 == data.results[randomquestion].correct_answer){
                 $(`#saveButton`).css(`display`, `unset`);
-                $(`#cocktailContainer`).css(`display`, `unset`);
+                $(`#cocktailContainer`).css(`display`, `block`);
                 clearanswer();
                 points++;
                 hidquestion();
@@ -142,7 +142,7 @@ starbtn.addEventListener ("click", function(){
         answ2.addEventListener("click", function(){
             if(actualresponse2 == data.results[randomquestion].correct_answer){
                 $(`#saveButton`).css(`display`, `unset`);
-                $(`#cocktailContainer`).css(`display`, `unset`);
+                $(`#cocktailContainer`).css(`display`, `block`);
                 clearanswer();
                 points++;
                 hidquestion();
@@ -158,7 +158,7 @@ starbtn.addEventListener ("click", function(){
         answ3.addEventListener("click", function(){
             if(actualresponse3 == data.results[randomquestion].correct_answer){
                 $(`#saveButton`).css(`display`, `unset`);
-                $(`#cocktailContainer`).css(`display`, `unset`);
+                $(`#cocktailContainer`).css(`display`, `block`);
                 clearanswer();
                 points++;
                 hidquestion();
@@ -174,7 +174,7 @@ starbtn.addEventListener ("click", function(){
         answ4.addEventListener("click", function(){
             if(actualresponse4 == data.results[randomquestion].correct_answer){   
                 $(`#saveButton`).css(`display`, `unset`); 
-                $(`#cocktailContainer`).css(`display`, `unset`);
+                $(`#cocktailContainer`).css(`display`, `block`);
                 clearanswer();
                 points++;
                 hidquestion();
